efeed780c10095b1c91d7d71d1cb3edb137d51cc
Berez0v <sasa59382@gmail.com>
2026-03-28 00:12:08 +0200

    pizda

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "92.113.22.21",
-  user: "u243158893_shunkin", // Измени на свой
-  password: "Shunkin123", // Измени на свой
-  database: "u243158893_shunkin", // Измени на свой
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
